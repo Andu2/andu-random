b1cde8e38ee4f87c989ce08e43eca43bc0aff0ba
Andy Iverson <aiguy42@gmail.com>
2018-05-23 20:07:47 -0500

    process data

---
+++ index.js
@@ -0,0 +1,11 @@
+const frequency = require("./data/processed/frequency.json");
+const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+function generateRandomWord() {
+	console.log("wow");
+}
+
+if (require.main === module) {
+	generateRandomWord();
+}
+
+module.exports = generateRandomWord;
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Random Word Generator
+
+See http://norvig.com/mayzner.html
+
+src/processData.js is how I formatted the data into JSON, index.js makes random words based on the data
